aaae98043c5949841f98706b109f9884de287ade
Kamil-87 <k.kamil87-87@yandex.ru>
2019-12-21 00:44:43 +0500

    refactoring code

--- lesson_05/chess/chessboard.js
+++ lesson_05/chess/chessboard.js
@@ -8,33 +8,47 @@
 
 const chessboard = {
     blackCellColor: '#000',
-    arrNumber: ['', 1, 2, 3, 4, 5, 6, 7, 8,''],
-    arrLetter: ['','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',''],
+    arrNumber: ['', 1, 2, 3, 4, 5, 6, 7, 8, ''],
+    arrLetter: ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ''],
+    table: {},
 
-    getChessBoard() {
-    document.body.insertAdjacentHTML('afterbegin', `<table id="table"></table>`);
-    const tableId = document.getElementById('table');
+    createChessBoard() {
+        this.createTable();
+        this.createCells();
+        this.initColors();
+    },
 
-    for(let i = 0; i <10; i++) {
-       const trElem = tableId.insertRow(i);
+    createTable() {
+        const tableId = 'table';
+        document.body.insertAdjacentHTML('afterbegin', `<table id=${tableId}></table>`);
+        this.table = document.getElementById(tableId);
+    },
 
-       for(let j = 0; j < 10; j++) {
-           const cell = trElem.insertCell(j);
-           if (i === 0) cell.innerText = this.arrLetter[j];
-           if(j === 0) cell.innerText = this.arrNumber[i];
+    createCells() {
+        for (let i = 0; i < this.arrNumber.length; i++) {
+            const trElem = this.table.insertRow(i);
 
-           if ( tableId.rows[0] && tableId.rows[9] && tableId.rows[i].cells[0] && tableId.rows[i].cells[9]) {
-               let symbolCells = document.querySelectorAll('tr:first-child > td, tr > td:first-child, tr:last-child > td, tr > td:last-child');
-               symbolCells.forEach( symbolCell =>  symbolCell.style = "background-color: #eee; border: none; text-align: center;");
-           }
-       }
-   }
-    let blackCells = document.querySelectorAll('tr:nth-child(n+2):nth-child(odd) > td:nth-child(even):nth-child(-n+8), tr:nth-child(even):nth-child(n+2):nth-child(-n+8) > td:nth-child(odd):nth-child(n+2)');
-    blackCells.forEach( blackCell => blackCell.style.backgroundColor = this.blackCellColor);
-   }
+            for (let j = 0; j < this.arrLetter.length; j++) {
+                const cell = trElem.insertCell(j);
+                if (i === 0) cell.innerText = this.arrLetter[j];
+                if (j === 0) cell.innerText = this.arrNumber[i];
+            }
+        }
+    },
+
+    initColors() {
+        // black cells
+        this.table.querySelectorAll('tr:nth-child(n+2):nth-child(odd) > td:nth-child(even):nth-child(-n+8), ' +
+            'tr:nth-child(even):nth-child(n+2):nth-child(-n+8) > td:nth-child(odd):nth-child(n+2)')
+            .forEach(blackCell => blackCell.style.backgroundColor = this.blackCellColor);
+
+        // borders
+        this.table.querySelectorAll('tr:first-child > td, tr > td:first-child, tr:last-child > td, tr > td:last-child')
+            .forEach(symbolCell => symbolCell.style = "background-color: #eee; border: none; text-align: center;");
+    }
 
 };
 
-window.addEventListener('load', () => chessboard.getChessBoard() );
+window.addEventListener('load', () => chessboard.createChessBoard());
 
 
